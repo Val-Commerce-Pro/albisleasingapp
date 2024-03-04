1a64267f8f32953b52d2fc87877873707a8cc42e
valdenicio22 <valdenicio22@gmail.com>
2024-03-04 17:15:00 +0100

    feat: update getMethodsData route

--- app/routes/api.getMethodsData.ts
+++ app/routes/api.getMethodsData.ts
@@ -1,78 +1,66 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
-type Methods =
+type Method =
   | "getZahlungsweisen"
   | "getProduktgruppen"
   | "getVertragsarten"
   | "getRechtsformen";
 
+const endPoint = `https://testschnittstelle.albis-leasing.de/Shop`;
+
+const getRequestTemplate = (method: Method) => ({
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    jsonrpc: "2.0",
+    method: method,
+    params: {
+      login: "test340534@t.de",
+      pwd: "Albis1234",
+    },
+    id: 1,
+  }),
+});
+
 export const loader: LoaderFunction = async ({ request }) => {
-  const requestedURL = new URL(request.url);
-
-  const getZahlungsweisen = requestedURL.searchParams.get("getZahlungsweisen");
-  const getProduktgruppen = requestedURL.searchParams.get("getProduktgruppen");
-  const getVertragsarten = requestedURL.searchParams.get("getVertragsarten");
-  const getRechtsformen = requestedURL.searchParams.get("getRechtsformen");
-
-  if (
-    !getZahlungsweisen ||
-    !getProduktgruppen ||
-    !getVertragsarten ||
-    !getRechtsformen
-  ) {
-    throw new Response("Bad Request", {
-      status: 400,
-    });
-  }
-  const methodsList: string[] = [
-    getProduktgruppen,
-    getVertragsarten,
-    getRechtsformen,
-    getZahlungsweisen,
+  const methods: Method[] = [
+    "getZahlungsweisen",
+    "getProduktgruppen",
+    "getVertragsarten",
+    "getRechtsformen",
   ];
 
-  const endPoint = `https://testschnittstelle.albis-leasing.de/Shop`;
-
-  const getRequestTemplate = (requestMethod: Methods) => {
-    return {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        method: requestMethod,
-        params: {
-          login: "test340534@t.de",
-          pwd: "Albis1234",
-        },
-        id: 1,
+  try {
+    const fetchDataPromises = methods.map((method) =>
+      fetch(endPoint, getRequestTemplate(method)).then((response) => {
+        console.log(`${method} response`, response);
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error! status: ${response.status} for method: ${method}`,
+          );
+        }
+        return response.json().then((data) => ({ method, data }));
       }),
-    };
-  };
+    );
+    console.log(`$fetchDataPromises - `, fetchDataPromises);
 
-  for (const method of methodsList) {
-    try {
-      const response = await fetch(
-        endPoint,
-        getRequestTemplate(method as Methods),
-      );
-      console.log(`${method} response`, response);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      console.log(`${method} data`, data);
-      return data;
-    } catch (error) {
-      console.error(`Error fetching ${method}:`, error);
-    }
+    const results = await Promise.all(fetchDataPromises);
+
+    console.log(`$results - `, results);
+
+    return json(results, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    // Handle errors, e.g., by returning a specific error response
+    throw new Response("Internal Server Error", {
+      status: 500,
+    });
   }
-
-  return json("It's work", {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
 };
