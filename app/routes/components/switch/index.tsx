60f86f6b62b164980fa221c4661e128cd4e8bd98
valdenicio22 <valdenicio22@gmail.com>
2024-03-13 01:42:13 +0100

    feat: refactor switch component

--- app/routes/components/switch/index.tsx
+++ app/routes/components/switch/index.tsx
@@ -1,38 +1,39 @@
 import type { ChangeEvent } from "react";
-import { useState } from "react";
+import { Label } from "../label";
 import styles from "./styles.module.css";
 
 type Switch = {
-  getCurrentValue: (isChecked: boolean) => void;
-  savedCheckboxValue?: boolean;
+  handleOnChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  checkboxValue?: boolean;
+  name: string;
+  label?: string;
 };
 
-export const Switch = ({ getCurrentValue, savedCheckboxValue }: Switch) => {
-  const [isChecked, setIsChecked] = useState<boolean>(
-    savedCheckboxValue ?? false,
-  );
-
-  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const currentValue = event.target.checked;
-    getCurrentValue(currentValue);
-    setIsChecked(currentValue);
-  };
-
+export const Switch = ({
+  handleOnChange,
+  checkboxValue,
+  label,
+  name,
+}: Switch) => {
   return (
-    <div className={styles.toggleSwitch}>
-      <span className={styles.toggleTextOff}>Off</span>
-      <input
-        type="checkbox"
-        id="toggle"
-        className={styles.toggleCheckbox}
-        onChange={handleOnChange}
-        checked={isChecked}
-      />
-      <label className={styles.toggleLabel} htmlFor="toggle">
-        <span className={styles.toggleInner}></span>
-        <span className={styles.toggleSwitch}></span>
-      </label>
-      <span className={styles.toggleTextOn}>On</span>
+    <div className={styles.switchContainer}>
+      {label && <Label label={label} />}
+      <div className={styles.toggleSwitch}>
+        <span className={styles.toggleTextOff}>Off</span>
+        <input
+          type="checkbox"
+          name={name}
+          id={`toggle-${name}`}
+          className={styles.toggleCheckbox}
+          onChange={(e) => handleOnChange(e)}
+          checked={checkboxValue}
+        />
+        <label className={styles.toggleLabel} htmlFor={`toggle-${name}`}>
+          <span className={styles.toggleInner}></span>
+          <span className={styles.toggleSwitch}></span>
+        </label>
+        <span className={styles.toggleTextOn}>On</span>
+      </div>
     </div>
   );
 };
