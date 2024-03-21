f06799db45c954eeef7d76a3ebeee2a121adc84f
valdenicio22 <valdenicio22@gmail.com>
2024-03-21 16:59:11 +0100

    feat: add extension with albis and albis request pages

---
+++ extensions/albis-leasing/assets/albis-leasing-script.js
@@ -0,0 +1,99 @@
+async function fillInterfaceWithMethodsData() {
+  const insurance = document.getElementById("propertyInsurance");
+  const financingAmount = document.getElementById("financingAmount");
+  const downPayment = document.getElementById("downPayment");
+  const calcBtn = document.getElementById("calculatorBtn");
+
+  const paymentMethod = document.getElementById("paymentMethod");
+  const shopDomainTag = document.getElementById("shopDomain");
+  const shop = shopDomainTag.textContent;
+
+  const getMethodsData = async (method, werte) => {
+    try {
+      //needs to add the credentials
+      const requestBody = werte ? { method, shop, werte } : { method, shop };
+
+      console.log("requestBody", requestBody);
+      const response = await fetch(
+        `https://albisleasingapp.cpro-server.de/api/getMethodsData`,
+        {
+          method: "POST",
+          body: JSON.stringify(requestBody),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching AppConfig:", error);
+    }
+  };
+
+  const mockWerte = {
+    kaufpreis: "769.00",
+    prodgrp: "1",
+    mietsz: "200.00",
+    vertragsart: "1",
+    zahlweise: "1",
+  };
+
+  const zahlungsweisen = await getMethodsData("getZahlungsweisen");
+  console.log("zahlungsweisen", zahlungsweisen);
+  const getRate = await getMethodsData("getRate", mockWerte);
+  console.log("getRate", getRate);
+
+  if (!zahlungsweisen) return console.log("Could not getzahlungsweisen Method");
+
+  if (paymentMethod) {
+    zahlungsweisen.result.forEach(({ bezeichnung, monate }) => {
+      const option = document.createElement("option");
+      option.text = bezeichnung;
+      option.value = monate;
+      paymentMethod.appendChild(option);
+    });
+  }
+
+  // function calculateLeasing(e) {
+  //   // Get values from the form
+
+  //   const testBtn = document.getElementById("testBtn");
+
+  //   testBtn.addEventListener("click", (e) => {
+  //     console.log("func testBtn");
+  //   });
+
+  //   calcBtn.addEventListener("click", (e) => {
+  //     e.preventDefault();
+  //     console.log(
+  //       "e, insurance, financingAmount, downPayment, paymentMethod",
+  //       e,
+  //       insurance,
+  //       financingAmount,
+  //       downPayment,
+  //       paymentMethod
+  //     );
+  //   });
+  // }
+}
+const getTestRoute = async () => {
+  try {
+    const requestUrl = `https://albisleasingapp.cpro-server.de/api/testRoute`;
+
+    const response = await fetch(requestUrl, { method: "GET" });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("getTestRoute:", data);
+  } catch (error) {
+    console.error("Error fetching AppConfig:", error);
+  }
+};
+document.addEventListener("DOMContentLoaded", async () => {
+  await getTestRoute();
+  await fillInterfaceWithMethodsData();
+});
---
+++ extensions/albis-leasing/assets/albis-request-script.js
@@ -0,0 +1,50 @@
+// alert("HELLO ALBIS LEASING REQUEST")
+
+function calculateLeasing(e) {
+  // Get values from the form
+  const insurance = document.getElementById("propertyInsurance").value;
+  const financingAmount = document.getElementById("financingAmount").value;
+  const downPayment = document.getElementById("downPayment").value;
+  const paymentMethod = document.getElementById("paymentMethod").value;
+  const calcBtn = document.getElementById("calculatorBtn");
+  const testBackend = document.getElementById("test-backend");
+
+  const testBtn = document.getElementById("testBtn");
+
+  testBtn.addEventListener("click", (e) => {
+    console.log("testBtn");
+  });
+
+  calcBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(
+      "e, insurance, financingAmount, downPayment, paymentMethod",
+      e,
+      insurance,
+      financingAmount,
+      downPayment,
+      paymentMethod,
+    );
+  });
+
+  testBackend.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("WOrks");
+  });
+
+  // fetch(url, {
+  //   method: "POST", // Specify the method
+  //   headers: {
+  //     "Content-Type": "application/json", // Specify the content type
+  //     // Add other headers as needed
+  //   },
+  //   body: JSON.stringify(data), // Convert the JavaScript object to a JSON string
+  // })
+  //   .then((response) => response.json()) // Parse the JSON response
+  //   .then((data) => {
+  //     console.log("Success:", data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error:", error);
+  //   });
+}
