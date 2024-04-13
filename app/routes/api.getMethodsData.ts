6db769e79a8403120d2a25eea2545de6e17b5283
valdenicio22 <valdenicio22@gmail.com>
2024-04-13 18:01:53 +0200

    wip - get contract

--- app/routes/api.getMethodsData.ts
+++ app/routes/api.getMethodsData.ts
@@ -1,55 +1,54 @@
 import type { ActionFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { getPluginConf } from "~/models/methods.server";
-import type { Antragsdaten, Method } from "./types/methods";
-import type { ModulZugangsdatenData } from "./types/pluginConfigurator";
-// import {
-//   getAlbisMethodsData,
-//   getRequestTemplate,
-//   type GetMethodsDataRequest,
-// } from "./utils/getAlbisMethodsData";
+// import type { Antragsdaten, Method } from "./types/methods";
+// import type { ModulZugangsdatenData } from "./types/pluginConfigurator";
+import {
+  getAlbisMethodsData,
+  // getRequestTemplate,
+  type GetMethodsDataRequest,
+} from "./utils/getAlbisMethodsData";
 
-type GetRate = {
-  kaufpreis: string;
-  prodgrp: string;
-  mietsz: string;
-  vertragsart: string;
-  zahlweise: string;
-};
-export interface GetMethodsDataRequest {
-  method: Method;
-  credentials?: ModulZugangsdatenData;
-  shop?: string;
-  werte?: GetRate;
-  antragsdaten?: Antragsdaten;
-  antragnr?: number;
-}
+// type GetRate = {
+//   kaufpreis: string;
+//   prodgrp: string;
+//   mietsz: string;
+//   vertragsart: string;
+//   zahlweise: string;
+// };
+// export interface GetMethodsDataRequest {
+//   method: Method;
+//   credentials?: ModulZugangsdatenData;
+//   shop?: string;
+//   werte?: GetRate;
+//   antragsdaten?: Antragsdaten;
+//   antragnr?: number;
+// }
 
-export const getRequestTemplate = (template: GetMethodsDataRequest) => {
-  const params = Object.entries(template).reduce(
-    (acc, [key, value]) => {
-      if (value !== undefined) {
-        acc[key] = value;
-      }
-      return acc;
-    },
-    {} as { [key: string]: unknown },
-  );
+// export const getRequestTemplate = (template: GetMethodsDataRequest) => {
+//   const params = Object.entries(template).reduce(
+//     (acc, [key, value]) => {
+//       if (value !== undefined) {
+//         acc[key] = value;
+//       }
+//       return acc;
+//     },
+//     {} as { [key: string]: unknown },
+//   );
 
-  console.log("params", params);
-  return {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      jsonrpc: "2.0",
-      method: template.method,
-      params,
-      id: 1,
-    }),
-  };
-};
+//   console.log("params", params);
+//   return {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       jsonrpc: "2.0",
+//       method: template.method,
+//       params,
+//       id: 1,
+//     }),
+//   };
+// };
 // const getRequestTemplate = (template: GetMethodsDataRequest) => {
 //   const params = Object.entries(template).reduce(
 //     (acc, [key, value]) => {
@@ -88,64 +87,64 @@ export const action: ActionFunction = async ({ request }) => {
   }: GetMethodsDataRequest = data;
   console.log("getMethodsBodyData", data);
   try {
-    const pluginConfData = shop && (await getPluginConf(shop));
-    const requestCredentials =
-      pluginConfData && pluginConfData.ModulZugangsdaten
-        ? {
-            apiLink: pluginConfData.ModulZugangsdaten.apiLink,
-            benutzer: pluginConfData.ModulZugangsdaten.benutzer,
-            passwort: pluginConfData.ModulZugangsdaten.passwort,
-          }
-        : credentials;
+    // const pluginConfData = shop && (await getPluginConf(shop));
+    // const requestCredentials =
+    //   pluginConfData && pluginConfData.ModulZugangsdaten
+    //     ? {
+    //         apiLink: pluginConfData.ModulZugangsdaten.apiLink,
+    //         benutzer: pluginConfData.ModulZugangsdaten.benutzer,
+    //         passwort: pluginConfData.ModulZugangsdaten.passwort,
+    //       }
+    //     : credentials;
 
-    if (!requestCredentials) {
-      return new Response("Invalid Credentials", {
-        status: 404,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-    }
+    // if (!requestCredentials) {
+    //   return new Response("Invalid Credentials", {
+    //     status: 404,
+    //     headers: {
+    //       "Access-Control-Allow-Origin": "*",
+    //     },
+    //   });
+    // }
 
-    const methodsPromise = await fetch(
-      requestCredentials.apiLink,
-      getRequestTemplate({
-        method,
-        credentials: requestCredentials,
-        werte,
-        antragsdaten,
-        antragnr,
-      }),
-    );
+    // const methodsPromise = await fetch(
+    //   requestCredentials.apiLink,
+    //   getRequestTemplate({
+    //     method,
+    //     credentials: requestCredentials,
+    //     werte,
+    //     antragsdaten,
+    //     antragnr,
+    //   }),
+    // );
 
-    if (!methodsPromise.ok) {
-      return new Response(
-        `HTTP error! status: ${methodsPromise.status} for method: ${method}`,
-        {
-          status: 404,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-          },
-        },
-      );
-    }
-    const methodsData = await methodsPromise.json();
-
-    // const methodsPromise = await getAlbisMethodsData({
-    //   method,
-    //   credentials,
-    //   werte,
-    //   shop,
-    //   antragsdaten,
-    //   antragnr,
-    // });
     // if (!methodsPromise.ok) {
-    //   throw new Error(
+    //   return new Response(
     //     `HTTP error! status: ${methodsPromise.status} for method: ${method}`,
+    //     {
+    //       status: 404,
+    //       headers: {
+    //         "Access-Control-Allow-Origin": "*",
+    //       },
+    //     },
     //   );
     // }
     // const methodsData = await methodsPromise.json();
-    // console.log("methodsData - getMethodsData Route - ", methodsData);
+
+    const methodsPromise = await getAlbisMethodsData({
+      method,
+      credentials,
+      werte,
+      shop,
+      antragsdaten,
+      antragnr,
+    });
+    if (!methodsPromise.ok) {
+      throw new Error(
+        `HTTP error! status: ${methodsPromise.status} for method: ${method}`,
+      );
+    }
+    const methodsData = await methodsPromise.json();
+    console.log("methodsData - getMethodsData Route - ", methodsData);
 
     return json(methodsData, {
       headers: {
--- app/routes/utils/getAlbisMethodsData.ts
+++ app/routes/utils/getAlbisMethodsData.ts
@@ -1,134 +1,93 @@
-// import { getPluginConf } from "~/models/methods.server";
+import { getPluginConf } from "~/models/methods.server";
 import type {
-  // Antragsdaten,
+  Antragsdaten,
   GetProduktgruppen,
   GetVertragsarten,
   GetZahlungsweisen,
+  Method,
 } from "../types/methods";
-// import type { ModulZugangsdatenData } from "../types/pluginConfigurator";
-
 import type { ModulZugangsdatenData } from "../types/pluginConfigurator";
-import { baseServerUrl } from "./urls";
 
-// type GetRate = {
-//   kaufpreis: string;
-//   prodgrp: string;
-//   mietsz: string;
-//   vertragsart: string;
-//   zahlweise: string;
-// };
-// export interface GetMethodsDataRequest {
-//   method: Method;
-//   credentials?: ModulZugangsdatenData;
-//   shop?: string;
-//   werte?: GetRate;
-//   antragsdaten?: Antragsdaten;
-//   antragnr?: number;
-// }
+type GetRate = {
+  kaufpreis: string;
+  prodgrp: string;
+  mietsz: string;
+  vertragsart: string;
+  zahlweise: string;
+};
+export interface GetMethodsDataRequest {
+  method: Method;
+  credentials?: ModulZugangsdatenData;
+  shop?: string;
+  werte?: GetRate;
+  antragsdaten?: Antragsdaten;
+  antragnr?: number;
+}
 
-// export const getRequestTemplate = (template: GetMethodsDataRequest) => {
-//   const params = Object.entries(template).reduce(
-//     (acc, [key, value]) => {
-//       if (value !== undefined) {
-//         acc[key] = value;
-//       }
-//       return acc;
-//     },
-//     {} as { [key: string]: unknown },
-//   );
+export const getRequestTemplate = (template: GetMethodsDataRequest) => {
+  const params = Object.entries(template).reduce(
+    (acc, [key, value]) => {
+      if (value !== undefined && key !== "method") {
+        acc[key] = value;
+      }
+      return acc;
+    },
+    {} as { [key: string]: unknown },
+  );
 
-//   console.log("params", params);
-//   return {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       jsonrpc: "2.0",
-//       method: template.method,
-//       params,
-//       id: 1,
-//     }),
-//   };
-// };
+  console.log("params", params);
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      method: template.method,
+      params,
+      id: 1,
+    }),
+  };
+};
 
-// export const getAlbisMethodsData = async ({
-//   method,
-//   antragnr,
-//   antragsdaten,
-//   credentials,
-//   shop,
-//   werte,
-// }: GetMethodsDataRequest) => {
-//   const pluginConfData = !credentials && shop && (await getPluginConf(shop));
-//   const requestCredentials =
-//     pluginConfData && pluginConfData.ModulZugangsdaten
-//       ? {
-//           apiLink: pluginConfData.ModulZugangsdaten.apiLink,
-//           benutzer: pluginConfData.ModulZugangsdaten.benutzer,
-//           passwort: pluginConfData.ModulZugangsdaten.passwort,
-//         }
-//       : credentials;
+export const getAlbisMethodsData = async ({
+  method,
+  antragnr,
+  antragsdaten,
+  credentials,
+  shop,
+  werte,
+}: GetMethodsDataRequest) => {
+  const pluginConfData = !credentials && shop && (await getPluginConf(shop));
+  const requestCredentials =
+    pluginConfData && pluginConfData.ModulZugangsdaten
+      ? {
+          apiLink: pluginConfData.ModulZugangsdaten.apiLink,
+          benutzer: pluginConfData.ModulZugangsdaten.benutzer,
+          passwort: pluginConfData.ModulZugangsdaten.passwort,
+        }
+      : credentials;
 
-//   if (!requestCredentials) throw new Error(`Invalid credentials`);
+  if (!requestCredentials) throw new Error(`Invalid credentials`);
 
-//   const methodsPromise = await fetch(
-//     requestCredentials.apiLink,
-//     getRequestTemplate({
-//       method,
-//       credentials: requestCredentials,
-//       werte,
-//       antragsdaten,
-//       antragnr,
-//     }),
-//   );
+  const methodsPromise = await fetch(
+    requestCredentials.apiLink,
+    getRequestTemplate({
+      method,
+      credentials: requestCredentials,
+      werte,
+      antragsdaten,
+      antragnr,
+    }),
+  );
 
-//   if (!methodsPromise.ok) {
-//     throw new Error(
-//       `HTTP error! status: ${methodsPromise.status} for method: ${method}`,
-//     );
-//   }
-//   const methodsData = await methodsPromise.json();
-//   return methodsData;
-// };
-
-// export const getAllMethodData = async (credentials: ModulZugangsdatenData) => {
-//   const [zahlungsweisen, produktgruppen, vertragsarten]: [
-//     GetZahlungsweisen,
-//     GetProduktgruppen,
-//     GetVertragsarten,
-//   ] = await Promise.all([
-//     getAlbisMethodsData({ method: "getZahlungsweisen", credentials }),
-//     getAlbisMethodsData({ method: "getProduktgruppen", credentials }),
-//     getAlbisMethodsData({ method: "getVertragsarten", credentials }),
-//   ]);
-
-//   return { zahlungsweisen, produktgruppen, vertragsarten };
-// };
-
-const getMethodsData = async (
-  method: string,
-  credentials: ModulZugangsdatenData,
-) => {
-  try {
-    const requestBody = { method, credentials };
-    const response = await fetch(`${baseServerUrl}/api/getMethodsData`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestBody),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching method data:", error);
-    return null;
+  if (!methodsPromise.ok) {
+    throw new Error(
+      `HTTP error! status: ${methodsPromise.status} for method: ${method}`,
+    );
   }
+  const methodsData = await methodsPromise.json();
+  return methodsData;
 };
 
 export const getAllMethodData = async (credentials: ModulZugangsdatenData) => {
@@ -137,10 +96,49 @@ export const getAllMethodData = async (credentials: ModulZugangsdatenData) => {
     GetProduktgruppen,
     GetVertragsarten,
   ] = await Promise.all([
-    getMethodsData("getZahlungsweisen", credentials),
-    getMethodsData("getProduktgruppen", credentials),
-    getMethodsData("getVertragsarten", credentials),
+    getAlbisMethodsData({ method: "getZahlungsweisen", credentials }),
+    getAlbisMethodsData({ method: "getProduktgruppen", credentials }),
+    getAlbisMethodsData({ method: "getVertragsarten", credentials }),
   ]);
 
   return { zahlungsweisen, produktgruppen, vertragsarten };
 };
+
+// const getMethodsData = async (
+//   method: string,
+//   credentials: ModulZugangsdatenData,
+// ) => {
+//   try {
+//     const requestBody = { method, credentials };
+//     const response = await fetch(`${baseServerUrl}/api/getMethodsData`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(requestBody),
+//     });
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching method data:", error);
+//     return null;
+//   }
+// };
+
+// export const getAllMethodData = async (credentials: ModulZugangsdatenData) => {
+//   const [zahlungsweisen, produktgruppen, vertragsarten]: [
+//     GetZahlungsweisen,
+//     GetProduktgruppen,
+//     GetVertragsarten,
+//   ] = await Promise.all([
+//     getMethodsData("getZahlungsweisen", credentials),
+//     getMethodsData("getProduktgruppen", credentials),
+//     getMethodsData("getVertragsarten", credentials),
+//   ]);
+
+//   return { zahlungsweisen, produktgruppen, vertragsarten };
+// };
