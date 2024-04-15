c12af27664a7f06e2519abd70298ccf1cc2a5eab
Dejan Simic <ds@commerce-pro.de>
2024-04-15 11:00:47 +0200

    Add types for request

--- extensions/dev-albis-leasing/src/types/localStorage.ts
+++ extensions/dev-albis-leasing/src/types/localStorage.ts
@@ -5,9 +5,37 @@ export type CalcData = {
   finanzierungsbetragNetto: string;
   anzahlung: string;
   zahlungsweise: string;
+  provision?: string;
+  prodgrp?: string;
+  vertragsart?: string;
 };
+
+export type CompanyManagerInfoData = {
+  anrede: string;
+  geburtsdatum: string;
+  nachname: string;
+  ortGF: string;
+  plzGF: string;
+  strasseGF: string;
+  telGF: string;
+  vorname: string;
+}
+
+export type CompanyInfoData = {
+  bank: string;
+  email: string;
+  firmenname: string;
+  ort: string;
+  plz: string;
+  rechtsform: string;
+  strasse: string;
+  telefon: string;
+}
 
 export interface LocalStorageI {
   calcData: CalcData;
   leasingRate: Rate;
+  companyInfoData: CompanyInfoData;
+  datenschutz: boolean;
+  companyManagerInfoData: CompanyManagerInfoData;
 }
