7760391cf13c1d640c735b399249ed8033f60055
Dejan Simic <ds@commerce-pro.de>
2024-04-18 13:07:48 +0200

    Add spinner component

--- extensions/dev-albis-leasing/src/components/modal/index.tsx
+++ extensions/dev-albis-leasing/src/components/modal/index.tsx
@@ -1,14 +1,17 @@
+import { Spinner } from "../spinner";
+
 type ModalProps = {
   onSubmit: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  isLoading: boolean;
 };
 
-export const Modal = ({ onSubmit }: ModalProps) => {
+export const Modal = ({ onSubmit, isLoading }: ModalProps) => {
   return (
     <div
       id="static-modal"
       data-modal-backdrop="static"
       tabIndex={-1}
-      className="hidden display overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
     >
       <div className="relative p-[16px] w-full max-w-[672px] max-h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -43,23 +46,23 @@ export const Modal = ({ onSubmit }: ModalProps) => {
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
               Sie sind kurz davor eine Leasinganfrage an ALBIS Leasing zu
               senden. Sind Sie sicher, dass ihre Daten korrekt sind? Die
-              Leasinganfrage kann im Nachhinein nicht zurückgezogen werden.
+              Leasinganfrage kann im Nachhinein nicht verändert werden.
             </p>
           </div>
-          <div className="flex items-center p-[16px] md:p-[20px] border-t border-gray-200 rounded-b dark:border-gray-600">
+          <div className="flex items-center p-[16px] md:p-[20px] border-t border-gray-200 rounded-b-[4px] dark:border-gray-600">
             <button
               onClick={(e) => onSubmit(e)}
               data-modal-hide="static-modal"
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-[20px] py-[10px] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="min-w-[145px] h-[48px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-[20px] py-[10px] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Anfrage senden
+              {isLoading ? <Spinner /> : "Anfrage senden"}
             </button>
             <button
               onClick={closeModal}
               data-modal-hide="static-modal"
               type="button"
-              className="py-[10px] px-[20px] ms-[12px] text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              className="h-[48px] py-[10px] px-[20px] ms-[12px] text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
               Abbrechen
             </button>
@@ -73,11 +76,11 @@ export const Modal = ({ onSubmit }: ModalProps) => {
 export function openModal() {
   const modal = document.getElementById("static-modal");
   if (!modal) return;
-  modal.classList.remove("hidden");
+  modal.style.display = "flex";
 }
 
 export function closeModal() {
   const modal = document.getElementById("static-modal");
   if (!modal) return;
-  modal.classList.add("hidden");
+  modal.style.display = "none";
 }
