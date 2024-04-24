d628977039586279de0ab33e3cc8c7d1c61f57d3
Dejan Simic <ds@commerce-pro.de>
2024-04-24 14:13:01 +0200

    Fix box gap being to large

--- extensions/dev-albis-leasing/src/components/box/index.tsx
+++ extensions/dev-albis-leasing/src/components/box/index.tsx
@@ -6,12 +6,13 @@ type BoxProps = {
   children: ReactNode;
   title?: string;
   toolTipContent?: string;
+  fill?: boolean
 };
 
-export const Box = ({ children, title, toolTipContent }: BoxProps) => {
+export const Box = ({ children, title, toolTipContent, fill }: BoxProps) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="w-full h-full flex flex-col ">
+    <div className={`w-full flex flex-col ${fill? "h-full" : ""}`}>
       {title && (
         <div className="bg-cyan-400 p-[16px] flex items-center justify-center rounded-t-lg">
           <h2 className="text-white font-semibold text-base text-center w-full">
